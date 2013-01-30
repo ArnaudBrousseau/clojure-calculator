9fe962c46907253875635af2e7d10333b840f96e
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2013-01-30 09:25:19 -0800

    Simple 'Hello Word'.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: lein run -m calculator.web $PORT
--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject heroku "1.0.0-SNAPSHOT"
+(defproject calculator "0.1"
   :description "A badass functional calculator"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-jetty-adapter "1.1.0"]])
