ab124ab3b67f704b835bcb5ecfceb64dfd1b1cc0
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2013-02-02 16:35:11 -0800

    Makes leiningen run at version 2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject calculator "0.1"
   :description "A badass functional calculator"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-jetty-adapter "1.1.0"]])
+                 [ring/ring-jetty-adapter "1.1.0"]]
+  :min-lein-version "2.0.0")
